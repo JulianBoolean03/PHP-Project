be6e06dca1953a652634f32b84fb38bb32917377
Amanda <anguyen224@student.gsu.edu>
2025-11-15 23:34:27 -0500

    Added php code to check if username already exists (not been tested yet)

---
+++ lobby.php
@@ -0,0 +1,20 @@
+<?php
+$errorMessage = $_GET['error'] ?? '';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Jeopardy - Homepage</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<div class = "page-wrapper">
+    <header class = "header">
+        <h1 class = "title">Jeopardy Game Show</h1>
+        <p class = "subtitle">Welcome to Jeopardy!</p>
+    </header>
+</div>
+</body>
+</html>
