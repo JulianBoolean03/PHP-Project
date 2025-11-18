258ad1d61434e816d816325f79195a475d8140cd
Amanda <anguyen224@student.gsu.edu>
2025-11-17 22:57:05 -0500

    Create question.php & put basic code for now

---
+++ question.php
@@ -0,0 +1,28 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Jeopardy - Question</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class = "page-wrapper">
+        <header class = "header">
+            <h1 class = "title">Jeopardy Game Show</h1>
+            <p class = "subtitle" >Game Time!</p>
+        </header>
+
+    <main class = "card">
+        <h2 class= "card-title">Question:</h2>
+        <!-- Section for question since there will be different questions leaving this blank for now -->
+    </main>
+
+    <footer class = "footer">
+        <p>Julian Robinson &amp; Amanda Nguyen</p>
+    </footer>
+    </div>
+</body>
+</html>
