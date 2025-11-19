2ba4b0e20287ff5a6874caaeac549a3792633aab
Amanda <anguyen224@student.gsu.edu>
2025-11-18 19:43:32 -0500

    Add php code for question.php

--- question.php
+++ question.php
@@ -1,4 +1,26 @@
 <?php
+
+$timer = 30; //Set timer to 30 seconds. May be changed to be longer/shorter if needed
+
+//Start timer when question shows up on screen
+session_start();
+if (!isset($_SESSION['question_start'])) {
+    $_SESSION['question_start'] = time();
+}
+
+//Get remaining time
+$time_elapse = time() - $_SESSION['question_start'];
+$time_remain = max(0, $timer - $time_elapse);
+
+//Automatically return to game board when timer ends
+if ($time_remain <= 0) {
+    unset($_SESSION['question_start']); //Resets timer for next question
+    header("Location: board.php"); //replace with name of game page if needed
+    exit();
+} //Put else for when time_remain is greater than 0
+
+//Get question from array
+$question = "Sample Question";
 ?>
 
 <!DOCTYPE html>
@@ -7,6 +29,9 @@
     <meta charset="UTF-8">
     <title>Jeopardy - Question</title>
     <link rel="stylesheet" href="styles.css">
+
+    <!-- Auto-refresh for PHP time-checking -->
+     <meta http-equiv="refresh" content="1">
 </head>
 <body>
     <div class = "page-wrapper">
@@ -18,6 +43,13 @@
     <main class = "card">
         <h2 class= "card-title">Question:</h2>
         <!-- Section for question since there will be different questions leaving this blank for now -->
+
+        <!-- Timer -->
+         <div class = "timer-wrapper">
+            <div class = "timer-bar"></div>
+         </div>
+
+         <p><?php echo htmlspecialchars($question); ?></p>
     </main>
 
     <footer class = "footer">
